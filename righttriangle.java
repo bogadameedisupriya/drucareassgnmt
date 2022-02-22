d16c3b39db0caf48416b4dc4fa04006ba0e08385
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-02-22 11:02:43 +0530

    patternprogram

---
+++ righttriangle.java
@@ -0,0 +1,33 @@
+package samplejava;
+
+public class righttriangle {
+
+	public static void main(String[] args)
+	{
+		int row=5;
+		int column=5;
+		for(int row1=1;row1<=5;row1++)
+		{
+			for(int column1=2*(5-row1); column1>=0;column1--)
+			{
+				System.out.print(" ");
+			}
+		  for(int column1=1;column1<=row1;column1++)
+		  {
+				System.out.print("* ");
+
+		  }
+			System.out.println();
+
+		//TODO Auto-generated method stub
+
+	}
+
+}
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+
