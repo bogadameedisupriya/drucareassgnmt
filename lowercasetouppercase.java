6399cb052a9c966aed344e38f73109393a1ae588
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-02-24 11:08:46 +0530

    Add files via upload

---
+++ lowercasetouppercase.java
@@ -0,0 +1,23 @@
+package samplejava;
+
+public class lowercasetouppercase {
+
+	public static void main(String[] args)
+	{
+		String s="java";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+	//	s=s.toUpperCase();
+	//	for(int i=0;i<1;i++)
+	//	{
+	//	System.out.println(s);
+		// TODO Auto-generated method stub
+	 // }
+	//	s=s.toLowerCase();
+	//	for(int i=0;i<1;i++)
+	//	{
+	//	System.out.println(s);
+		// TODO Auto-generated method stub
+	}
+	}
+
