4412177d4cb6ee7e864ea8e0e6141673fed04a63
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-03-05 12:23:02 +0530

    Add files via upload

---
+++ smallestnumber.java
@@ -0,0 +1,19 @@
+package samplejava;
+
+public class smallestnumber {
+
+	public static void main(String[] args)
+	{
+		int n1=9,n2=10;
+		if(n1>n2)
+		{
+			System.out.println(n2);
+		}else
+		{
+			System.out.println(n1);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
