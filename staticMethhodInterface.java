3185126b7a4686c19760fc738dda540d2a077db5
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-03-05 12:16:44 +0530

    Add files via upload

---
+++ staticMethhodInterface.java
@@ -0,0 +1,21 @@
+package samplejava;
+
+public  class staticMethhodInterface implements defaultMethodInterface {
+
+	public static void main(String[] args) {
+		defaultMethodInterface d1=new staticMethhodInterface();
+	//	d1.i1();
+		d1.i2();
+		defaultMethodInterface.i3();
+		defaultMethodInterface.i4();
+		// TODO Auto-generated method stub
+
+	}
+
+//	@Override
+//	public void i1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+//}
