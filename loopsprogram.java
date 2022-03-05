50578dbe8838a25687ba14328b89c217d572c224
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-03-05 12:04:51 +0530

    Add files via upload

---
+++ loopsprogram.java
@@ -0,0 +1,46 @@
+package basicjavaprograms;
+
+public class loopsprogram {
+
+	public static void main(String[] args)
+	{
+		int i=20;
+		if(i>30) {
+					System.out.println("true");
+			
+				}else {
+					System.out.println("false");
+				}
+		int j=20;
+		while(j<10);
+		{
+			System.out.println(j);
+			j++;
+		}
+		do while(j>10)
+		{
+			j++;
+			System.out.println(j);
+			break;
+		}while(j<10);
+	}
+}
+	
+	
+	
+	
+	
+		
+
+			
+		
+		
+			
+		
+			
+			
+		// TODO Auto-generated method stub
+
+	
+
+
