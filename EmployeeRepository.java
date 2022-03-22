e3349479796c2f25f7ead74d961766000b89df62
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-03-22 15:48:06 +0530

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.drucare.Entity.Employee;
-
-public interface EmployeeRepository extends CrudRepository< Employee, String>{ 
-
-}
