df7d2d1f42b20dbab9c75296ffb9eba3dfa16c20
bogadameedisupriya <99254631+bogadameedisupriya@users.noreply.github.com>
2022-03-22 15:52:18 +0530

    project

---
+++ EmployeeCourseApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmployeeCourseApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EmployeeCourseApplication.class, args);
+
+	}
+
+}
